c7aab3b2cf1dfc162354833d4f336aa99cf1f2e4
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-08-24 22:14:22 +0530

    lab-6 B1 added

--- lab-6/B1.c
+++ lab-6/B1.c
@@ -4,3 +4,22 @@
 - For next 100 units Rs. 1.20/unit 
 - For unit above 250 Rs. 1.50/unit 
 - An additional surcharge of 20% is added to the bill. */
+
+#include<stdio.h>
+int main(){
+    float unit,price,total;
+    printf("Enter units:");
+    scanf("%f",&unit);
+    if(unit<=50){
+        price = unit*0.50;
+    }else if(unit<=150){
+        price = 50*0.50 + (unit-50)*0.75;
+    }else if(unit<=250){
+        price = 50*0.50 + 100*0.75 + (unit-150)*1.20;
+    }else{
+        price = 50*0.50 + 100*0.75 + 250*1.20 + (unit-250)*1.50 ;
+    }
+    total=(price*20)/100;
+    printf("%f",total);
+    return 0;
+}
