ef9c91be15fe3ceeb2c0726dd12f681e335ecf20
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-08-24 22:11:22 +0530

    lab-6 A3 added

--- lab-6/A3.c
+++ lab-6/A3.c
@@ -1,1 +1,21 @@
 //Check whether the entered character is upper case, lower case, digit or any special character.
+
+#include<stdio.h>
+int main(){
+    char ch;
+
+    printf("Enter character:");
+    scanf("%c",&ch);
+
+    if(ch>='A'&&ch<='Z'){
+        printf("Upper case");
+    }else if(ch>='a'&&ch<='z'){
+        printf("lower case");
+    }else if(ch>='0'&&ch<='9'){
+        printf("digit");
+    }else{
+        printf("special character");
+    }
+
+    return 0;
+}
