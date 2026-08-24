ed78816b2ba8cd6fe8f13d9118016fcca4e95657
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-08-24 22:12:41 +0530

    lab-6 A4 added

--- lab-6/A4.c
+++ lab-6/A4.c
@@ -1,1 +1,19 @@
 //Input an integer number and check the last digit of number is even or odd.
+
+#include<stdio.h>
+int main(){
+    int n,last;
+
+    printf("Enter n:");
+    scanf("%d",&n);
+
+    last=n%10;
+
+    if(last%2==0){
+        printf("even");
+    }else{
+        printf("odd");
+    }
+    
+    return 0;
+}
