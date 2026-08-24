1ee02672a4bd45cb04219882b125a319af47ea87
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-08-24 22:45:47 +0530

    lab-6 C1 added

--- lab-6/C1.c
+++ lab-6/C1.c
@@ -1,1 +1,18 @@
 //Find the second largest number among three user input numbers.
+
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter three number : ");
+    scanf("%d %d %d",&a,&b,&c);
+    if((a >= b && a <= c) || (a <= b && a >= c)){
+        printf("%d",a);
+    }
+    else if((b >= a && b <= c) || (b <= a && b >= c)){
+        printf("%d",b);
+    }
+    else{
+        printf("%d",c);
+    }
+    return 0;
+}
--- lab-6/C2.c
+++
@@ -1,6 +0,0 @@
-/* In digital world colors are  specified in RGB format, with values of R, G, and B varying on integer scale 
-from 0 to 255. Colors are mentioned in Cyan-Magenta-Yellow-Black (CMYK) format with values of C, M, 
-Y  and  K  varying  on  a  real  scale  from  0.0  to  1.0.  Convert  RGB  color  to  CMYK  as  per  formula:  - 
-White=Max(red/255,green/255,blue/255) - Cyan=(white - red/255)/white - Magenta=(white - 
-green/255)/white - Yellow=(white - blue/255)/white - Black=1 - white Note: if RGB values are all 0, then 
-the CMY values are all 0 and the K value is 1.*/
