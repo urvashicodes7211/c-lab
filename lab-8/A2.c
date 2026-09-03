ed8ff9e2f37268d83023bbf8f410e07b98e1305d
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-09-03 14:30:20 +0530

    lab-8 A2 added

---
+++ lab-8/A2.c
@@ -0,0 +1,25 @@
+// Print odd numbers between 1 to 10 then modify 1 to n using while and do while loop.
+#include<stdio.h>
+int main(){
+    int i=1; 
+
+    while(i<=10){
+        if(i%2!=0){
+            printf("%d\n",i);
+        }
+        i++;
+    }
+
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+
+    i=1;
+    while(i<=n){
+         if(i%2!=0){
+            printf("%d\n",i);
+        }
+        i++;
+    }
+    return 0;
+}
